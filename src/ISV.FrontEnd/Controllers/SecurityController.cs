c357eb5c756790f2422001ea9cf5e8cd76adc4fa
PENTEGY-UA\sergey.ilminskyi <sergey.ilminskyi@pentegyservices.com>
2018-10-01 17:52:07 +0300

    update

--- src/ISV.FrontEnd/Controllers/SecurityController.cs
+++ src/ISV.FrontEnd/Controllers/SecurityController.cs
@@ -2,11 +2,73 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Security;
 using System.Web.Mvc;
+using ISV.FrontEnd.Models;
 
 namespace ISV.FrontEnd.Controllers
 {
     public class SecurityController : Controller
     {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // поиск пользователя в бд
+                
+                if (true)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Login, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователя с таким логином и паролем нет");
+                }
+            }
+
+            return View(model);
+        }
+
+        public ActionResult Logoff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Security");
+        }
+
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (true)
+                {
+                    FormsAuthentication.SetAuthCookie(model.EMail, true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Пользователь с таким логином уже существует");
+                }
+            }
+
+            return View(model);
+        }
     }
 }
---
+++ src/ISV.FrontEnd/Models/Security/RegisterModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ISV.FrontEnd.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string EMail { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string ConfirmPassword { get; set; }
+    }
+}
